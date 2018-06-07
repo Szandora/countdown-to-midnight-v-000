3fc966f96ec4fb17906dd3fe93583a7eb0c2c464
Sheila Wittman <szandora789@gmail.com>
2018-06-07 20:41:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,9 @@ def countdown(number)
   while number != 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    countdown_with_sleep
-    puts "HAPPY NEW YEAR!"
+    countdown_with_sleep (1)
   end
+  puts "HAPPY NEW YEAR!"
 end
 
 
